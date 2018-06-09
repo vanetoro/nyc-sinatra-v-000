7934d7cee485b766e44694dbc5dee32e6400e527
Vanessa Toro <vanetoro84@gmail.com>
2018-06-09 17:53:14 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,6 @@
 class FiguresController < ApplicationController
 
-  get '/' do
+  get '/figures' do
     @figures = Figure.all
 
     erb :'figures/index'
@@ -11,22 +11,38 @@ class FiguresController < ApplicationController
     erb :'figures/new'
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    # binding.pry
+    erb :'figures/show/'
+  end
+
   post '/figures' do
     @figure = Figure.create(name: params[:figure][:name])
     @landmarks = params[:figure][:landmark_ids]
     @titles = params[:figure][:title_ids]
-     if @landmarks
-       @landmarks.each do
-      @figure.landmarks <<  Landmark.find(l_id)
-    end
+    @new_title = params[:title][:name]
+    @new_landmark = params[:landmark][:name]
+      if @landmarks
+         @landmarks.each do |l_id|
+           @figure.landmarks <<  Landmark.find(l_id)
+        end
+      end
     if @titles
-      @titles.each do |l_id|
-        @figure.titles <<  Title.find(l_id)
+      @titles.each do |t_id|
+        @figure.titles <<  Title.find(t_id)
+      end
     end
+    if !@new_title.empty?
+      @title = Title.create(name: @new_title)
+      @figure.titles << @title
+    end
+    if !@new_landmark.empty?
+      @landmark = Landmark.create(name: @new_landmark)
+      @figure.landmarks << @landmark
+    end
+    @figure.save
+    redirect "figures/#{@figure.id}"
   end
-
-  end
-
-
 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,23 @@
 class LandmarksController < ApplicationController
 
+    get '/' do
+      @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+
+    erb :'/landmarks/new'
+  end
+
+  get 'landmarks/:slug/edit' do
+
+  end
+
+  get '/landmarks/:slug' do
+
+    erb :'/landmarks/show'
+
+  end
+
 end
