460af89bba64cd42328770a79f3ce8696165448c
Vanessa Toro <vanetoro84@gmail.com>
2018-06-09 20:05:29 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -37,7 +37,7 @@ class FiguresController < ApplicationController
       @figure.landmarks << Landmark.find_or_create_by(name: @landmark)
     end
     @figure.save
-    redirect "figures/#{@figure.id}"
+    redirect "figure/#{@figure.id}"
   end
 
   post '/figures' do
